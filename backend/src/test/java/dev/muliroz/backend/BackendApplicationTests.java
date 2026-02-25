97a738f55beed687ff81906a39c662ae7a620323
muliroZ <murilomega100@gmail.com>
2026-02-25 18:08:27 -0300

    Initial commit

---
+++ backend/src/test/java/dev/muliroz/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.muliroz.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
