029226f876fb6244b4c70a99787c84c7e9d7b1f0
muliroZ <murilomega100@gmail.com>
2026-02-26 17:39:06 -0300

    security filter implementation

---
+++ backend/src/main/java/dev/muliroz/backend/infrastructure/security/CustomUserDetailsService.java
@@ -0,0 +1,32 @@
+package dev.muliroz.backend.infrastructure.security;
+
+import dev.muliroz.backend.domain.entity.User;
+import dev.muliroz.backend.infrastructure.persistence.repository.JpaUserRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final JpaUserRepository repository;
+
+    public CustomUserDetailsService(JpaUserRepository repository) {
+        this.repository = repository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = repository.findByEmail(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+
+        return org.springframework.security.core.userdetails.User.builder()
+                .username(user.getEmail())
+                .password(user.getPassword())
+                .authorities(new ArrayList<>())
+                .build();
+    }
+}
---
+++ backend/src/main/java/dev/muliroz/backend/infrastructure/security/JwtAuthFilter.java
@@ -0,0 +1,57 @@
+package dev.muliroz.backend.infrastructure.security;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+public class JwtAuthFilter extends OncePerRequestFilter {
+
+    private final JwtGenerator jwtGenerator;
+    private  final CustomUserDetailsService service;
+
+    public JwtAuthFilter(JwtGenerator jwtGenerator, CustomUserDetailsService service) {
+        this.jwtGenerator = jwtGenerator;
+        this.service = service;
+    }
+
+    @Override
+    protected void doFilterInternal(
+            HttpServletRequest request, HttpServletResponse response, FilterChain filterChain
+    ) throws ServletException, IOException {
+
+        String authHeader = request.getHeader("Authorization");
+
+        if (authHeader == null || authHeader.startsWith("Bearer ")) {
+            filterChain.doFilter(request, response);
+            return;
+        }
+
+        String token = authHeader.substring(7);
+        String subject = jwtGenerator.getSubjectFromToken(token);
+
+        if (subject != null && SecurityContextHolder.getContext().getAuthentication() == null) {
+            UserDetails userDetails = service.loadUserByUsername(subject);
+
+            if (jwtGenerator.validateToken(token, subject)) {
+                UsernamePasswordAuthenticationToken auth = new UsernamePasswordAuthenticationToken(
+                        userDetails.getUsername(),
+                        null,
+                        userDetails.getAuthorities()
+                );
+                auth.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+                SecurityContextHolder.getContext().setAuthentication(auth);
+            }
+        }
+        filterChain.doFilter(request, response);
+    }
+}
